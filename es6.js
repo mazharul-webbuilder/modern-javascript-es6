de00205b0f299fe4e5656b62e8c5afdd55627d6b
Mazharul Islam <rafi1998.bdr@gmail.com>
2023-09-19 21:12:16 +0600

    arrow function

---
+++ es6.js
@@ -0,0 +1,27 @@
+/*Arrow Function*/
+function normalFunction()
+{
+    return 10
+}
+
+let arrowFunction = () => {
+    return 10
+}
+let arrowFunSum = (a, b) => a + b
+
+let arrowSingleParam = a => a
+
+let arrowFunctionOptimize = () => 10;
+
+let largeArrowFunction = () => {
+    /*your .... code*/
+    return 20
+}
+
+/*Truly or Falsely Value*/
+var myVar = true;
+if (myVar){
+    console.log('I am truthy')
+} else {
+    console.log('I am falsely')
+}
